860dfb3a81a0b8a58015814fba904fc4c0741620
xpham <xuanhoa.pham@gmail.com>
2018-08-23 14:15:17 -0400

    First commit after branching out

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,13 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
+    // CommonModule is a module that provides all kinds of services and directives one
+    // usually wants to use in an Angular2 application like ngIf.
+    // CommonModule is platform-independent.
+    // BrowserModule exports CommonModule and provides a few services specific
+    // to the browser platform (in contrary to ServerModule or ServiceWorkerModule).
+    // BrowserModule should only be imported in AppModule, CommonModule can be imported everywhere.
     BrowserModule
   ],
   bootstrap: [AppComponent]
